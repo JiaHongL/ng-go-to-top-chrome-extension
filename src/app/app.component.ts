a3208523cdd26597b7577de63bbd117b6913aa8f
joe <>
2023-04-15 14:45:34 +0800

    實作一個簡易的 go to top 元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,22 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+
+import { GoToTopComponent } from './go-to-top/go-to-top.component';
 
 @Component({
     selector: 'app-root',
     template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+      <!-- <ng-container *ngFor="let item of fakeArray;let i = index;">
+        <p>測試滾軸是否正常 {{i}} </p>
+      </ng-container> -->
+      <app-go-to-top><app-go-to-top>
   `,
     styles: [],
-    standalone: true
+    standalone: true,
+    imports: [GoToTopComponent, NgFor, NgIf]
 })
 export class AppComponent {
-  title = 'ng-go-to-top-chrome-extension';
+  fakeArray = Array(200).fill('');
 }
+
+
---
+++ src/app/go-to-top/go-to-top.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-go-to-top',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <button class="btn-color go-to-top" (click)="goToTop()">回到頂部</button>
+  `,
+  styles: [`
+  .go-to-top {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    color: #ffffff;
+    border: none;
+    border-radius: 4px;
+    padding: 10px 15px;
+    cursor: pointer;
+    z-index: 1000;
+  }
+
+  .go-to-top:hover {
+      background-color: #1e88e5;
+  }
+
+  .btn-color {
+      background-color: #2196f3;
+  }
+`]
+})
+export class GoToTopComponent {
+
+  document = inject(DOCUMENT);
+
+  constructor() { }
+
+  goToTop() {
+    // this.document.documentElement.scrollTo({ top: 0, behavior: 'smooth' });
+    this.document.querySelector('main')?.children[0].children[0].children[0].scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+}
